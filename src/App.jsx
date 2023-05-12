7af153c8a9c6c0faaebb3844ada424644bd4c9f7
Siam3h <philosiama@gmail.com>
2023-05-12 22:08:48 +0300

    Feat:Updated the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
 
-import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import LogIn from "./components/LogIn"
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Checkout from "./pages/Checkout/Checkout";
+import LogIn from "./components/LogIn";
+import Dashboard from "./components/Dashboard";
+import Register from "./components/Register";
+import Buy from "./components/Buy";
+import Sell from "./components/sell";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Box } from "@chakra-ui/react";
+
 
 
 function App() {
 
 
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home />}>
-          <Route path="login" element={<LogIn />} />
-        </Route>
-      </Routes>
-    </>
+
+    <Router>
+      <Box h="100vh">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/buy" element={<Buy />} />
+          <Route path="/sell" element={<Sell />} />
+        </Routes>
+      </Box>
+    </Router>
   )
 }
 
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -16,6 +16,7 @@ import {
     Flex,
     Image
 } from '@chakra-ui/react';
+import NavBar from "../components/NavBar";
 
 const Dashboard = () => {
 
@@ -23,6 +24,7 @@ const Dashboard = () => {
     return (
         <>
             <Box boxShadow="0px 2px 20px rgba(0, 0, 0, 0.05)" p="25px" m="15px">
+                <NavBar />
                 <Flex justify="space-between" mr="45px" ml="45px" mt="45px" p="25px">
                     <Flex flexDirection="column" gap="10px">
                         <Tooltip
@@ -50,56 +52,56 @@ const Dashboard = () => {
                         </Button>
                     </Box>
                 </Flex>
-                {isProfileCreated ? 
-                <Box m="50px 250px" boxShadow="0px 2px 20px rgba(0, 0, 0, 0.05)">
-                    <TableContainer>
-                        <Table>
-                            <Thead>
-                                <Tr>
-                                    <Th>Sacco Name</Th>
-                                    <Th>Shares Volume</Th>
-                                    <Th></Th>
-                                </Tr>
-                            </Thead>
-                            <Tbody>
-                                <Tr>
-                                    <Td>Stima Sacco</Td>
-                                    <Td>5000</Td>
-                                    <Td ><IconButton w='1px' h='10px' borderRadius="50%" >
-                                        <Center w='25px' h='25px' p="15px" borderRadius="50%" bg="#17a589" color='white'>
-                                            <ArrowRightIcon />
-                                        </Center>
-                                    </IconButton></Td>
-                                </Tr>
-                                <Tr>
-                                    <Td>Magereza Sacco</Td>
-                                    <Td>6575</Td>
-
-                                    <Td>
-                                        <IconButton w='1px' h='10px' borderRadius="50%" >
+                {isProfileCreated ?
+                    <Box m="50px 250px" boxShadow="0px 2px 20px rgba(0, 0, 0, 0.05)">
+                        <TableContainer>
+                            <Table>
+                                <Thead>
+                                    <Tr>
+                                        <Th>Sacco Name</Th>
+                                        <Th>Shares Volume</Th>
+                                        <Th></Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    <Tr>
+                                        <Td>Stima Sacco</Td>
+                                        <Td>5000</Td>
+                                        <Td ><IconButton w='1px' h='10px' borderRadius="50%" >
                                             <Center w='25px' h='25px' p="15px" borderRadius="50%" bg="#17a589" color='white'>
                                                 <ArrowRightIcon />
                                             </Center>
-                                        </IconButton>
-                                    </Td>
-                                </Tr>
-                                <Tr>
-                                    <Td>Mwalimu Sacco</Td>
-                                    <Td>2550</Td>
-                                    <Td>
-                                        <IconButton w='1px' h='10px' borderRadius="50%" >
-                                            <Center w='25px' h='25px' p="15px" borderRadius="50%" bg="#17a589" color='white'>
-                                                <ArrowRightIcon />
-                                            </Center>
-                                        </IconButton>
-                                    </Td>
-                                </Tr>
+                                        </IconButton></Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td>Magereza Sacco</Td>
+                                        <Td>6575</Td>
 
-                            </Tbody>
-                        </Table>
-                    </TableContainer>
-                </Box>
-                : <Flex align="center" justify="center"> <Image  src="../../public/illustrationmoney.png" boxSize="5000px 5000px" /> </Flex>}
+                                        <Td>
+                                            <IconButton w='1px' h='10px' borderRadius="50%" >
+                                                <Center w='25px' h='25px' p="15px" borderRadius="50%" bg="#17a589" color='white'>
+                                                    <ArrowRightIcon />
+                                                </Center>
+                                            </IconButton>
+                                        </Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td>Mwalimu Sacco</Td>
+                                        <Td>2550</Td>
+                                        <Td>
+                                            <IconButton w='1px' h='10px' borderRadius="50%" >
+                                                <Center w='25px' h='25px' p="15px" borderRadius="50%" bg="#17a589" color='white'>
+                                                    <ArrowRightIcon />
+                                                </Center>
+                                            </IconButton>
+                                        </Td>
+                                    </Tr>
+
+                                </Tbody>
+                            </Table>
+                        </TableContainer>
+                    </Box>
+                    : <Flex align="center" justify="center"> <Image src="../../public/illustrationmoney.png" boxSize="5000px 5000px" /> </Flex>}
             </Box>
         </>
     )
